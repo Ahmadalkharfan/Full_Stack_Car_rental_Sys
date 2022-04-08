6d92435c01a0439a8d8460babe6b284b362595f3
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2022-04-09 06:23:14 +0800

    made a code to auto generate records

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const dbConfig = require('../config/dbConfig.js');
 const {Sequelize, DataTypes} = require('sequelize');
-
+var mysql = require('mysql2');
 
 const sequelize = new Sequelize(
     dbConfig.DB,
@@ -18,6 +18,16 @@ const sequelize = new Sequelize(
         }
     }
 )
+
+// Creating a mysql connection
+var con = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+ });
+
+
 
 sequelize.authenticate()
 .then(() => {
@@ -65,6 +75,30 @@ db.car_availability.belongsTo(db.cars,{
     foreignKey: 'car_id',
     as: 'car'
 })
+
+//inserting values for testing
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+    var sql = "INSERT INTO Users (id, username, firstName, lastName, birthday, gender, role, pass, mobileNumber, createdAt, updatedAt)  VALUES (NULL, 'Ahmad', 'Ahmad', 'Alkharfan', '2022-04-20', 'M', 'Admin', 'password!', '019', '2022-04-08 23:15:54.000000', '2022-04-08 23:15:54.000000'),(NULL, 'Andrew', 'Andrew', 'Alkharfan', '2022-04-20', 'M', 'Admin', 'password!', '019', '2022-04-08 23:15:54.000000', '2022-04-08 23:15:54.000000'),(NULL, 'Danny', 'Danny', 'Alkharfan', '2022-04-20', 'M', 'User', 'password!', '019', '2022-04-08 23:15:54.000000', '2022-04-08 23:15:54.000000');";
+    con.query(sql, function (err, result) {
+       if (err) throw err;
+       console.log("Successfully inserted multiple records into the table.");
+    });
+
+    var sql = "INSERT INTO Cars (id, brand, door_num, year, color, model_name, mode, plate_number, user_id, location_latitudes, location_longitude, day_price, is_featured, createdAt, updatedAt)   VALUES (NULL, 'KIA', '4', '2022', 'Red', 'Rio', 'AUTO', 'KYK123', '1', '22.22', '-33.22', '20', '0', '2022-04-09 00:07:29.000000', '2022-04-09 00:07:29.000000'),(NULL, 'Audi', '2', '2022', 'Red', 'A3', 'AUTO', 'KYK122', '2', '22.22', '-33.22', '20', '0', '2022-04-09 00:07:29.000000', '2022-04-09 00:07:29.000000'),(NULL, 'KIA', '5', '2020', 'Blue', 'Rio', 'MANUAL', 'KYK123', '3', '22.22', '-33.22', '20', '0', '2022-04-09 00:07:29.000000', '2022-04-09 00:07:29.000000');";
+    con.query(sql, function (err, result) {
+       if (err) throw err;
+       console.log("Successfully inserted multiple records into the table.");
+    });
+
+    var sql = "INSERT INTO carAvailabilities (id, car_id, start_at, end_at, createdAt, updatedAt)  VALUES (NULL, '1', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000'),(NULL, '2', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000'),(NULL, '3', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000', '2022-04-09 00:12:53.000000');";
+    con.query(sql, function (err, result) {
+       if (err) throw err;
+       console.log("Successfully inserted multiple records into the table.");
+    });
+ });
+
 
 module.exports = db
 
